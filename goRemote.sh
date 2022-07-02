b741fb4f9da40016d1d24982c371e3d736f56e14
Rahul Kumar <kum28ra@gmail.com>
2022-07-02 14:12:33 +0530

    abcd

--- goRemote.sh
+++ goRemote.sh
@@ -1,8 +1,20 @@
+trap ctrl_c INT
+
+function ctrl_c(){
+        echo "Stopping services"
+	sudo systemctl stop ssh
+	sudo ufw deny ssh
+}
+
+
+sudo systemctl start ssh
+sudo ufw allow ssh
+
 nohup ngrok tcp 22 &
 sleep 3
 url=`curl -s  --location --request GET 'localhost:4040/api/tunnels' | jq .tunnels[0].public_url`
 echo $url
-curl --location --request POST 'https://go-remote-ssh.herokuapp.com/post2' --header "address: $url"
+curl --location --request POST 'https://go-remote-ssh.herokuapp.com/post' --header "address: $url"
 echo ""
 while [ "$url" != "" ]
 do
